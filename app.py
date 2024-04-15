adf5aea2997ad7d38482b1a4f2afd53b547e90f5
alexandre camargo <xandecamargom@gmail.com>
2024-04-15 13:18:18 -0300

    rota do 2000

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from funcoes import maior_de_50, mais_2000
+from randomdata import pessoas
 
 app = Flask(__name__)
 @app.route("/", methods=("GET",))
 def index():
-    return jsonify({"status": 200, "message": "API Isabella Galvão "})
+    return jsonify({"status": 200, "message": "API Alexandre Camargo "})
 
 
 @app.route("/aleatorios")
@@ -19,6 +21,16 @@ def argumentos(nome: str):
 @app.route("/argumentos", methods=("GET",))
 def arg_implicito():
     return jsonify({"status": 200, "data": request.args["nome"]})
+
+@app.route("/maior_de_cinquenta", methods = ["GET"])
+def maior_que_cinquenta():
+    maior_de_cinquenta = maior_de_50(pessoas)
+    return jsonify({"Status":200, "mensagem":maior_de_cinquenta})
+
+@app.route("/mais_doismil", methods = ["GET"])
+def mais_de_doismil():
+    maisdoismil = mais_2000(pessoas)
+    return jsonify({"Status":200, "mensagem":maisdoismil})
 
 if __name__ == '__main__':
     app.run(debug=True)
