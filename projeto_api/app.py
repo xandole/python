32ced856b2a6ff79c5299daab2c9f2473887f869
xandole <159169259+xandole@users.noreply.github.com>
2024-04-15 14:06:31 -0300

    Add files via upload

---
+++ projeto_api/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+@app.route("/", methods=("GET",))
+def index():
+    return jsonify({"status": 200, "message": "API Isabella Galvão "})
+
+
+@app.route("/aleatorios")
+def aleatorios():
+    import random
+    a = random.randint(49, 100)
+    return jsonify({"status": 200, "data": a})
+
+@app.route("/argumentos/<string:nome>", methods=("GET",))
+def argumentos(nome: str):
+    return jsonify({"status": 200, "data": nome})
+
+@app.route("/argumentos", methods=("GET",))
+def arg_implicito():
+    return jsonify({"status": 200, "data": request.args["nome"]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
